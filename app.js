d98ff3f1122d89802e05fba84e7c31b56094b500
bunnyBites <s.mukeshkr481@gmail.com>
2023-03-13 21:05:42 +0530

    added todo list styles and complete functionality

--- app.js
+++ app.js
@@ -8,38 +8,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // setting current view engine as ejs
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  const currentDay = new Date().getDay();
-  let dayType = "";
+// set public folder as static file holder
+app.use(express.static("public"));
 
-  switch (currentDay) {
-    case 0:
-      dayType = "Sunday";
-      break;
-    case 1:
-      dayType = "Monday";
-      break;
-    case 2:
-      dayType = "Tuesday";
-      break;
-    case 3:
-      dayType = "Wednesday";
-      break;
-    case 4:
-      dayType = "Thursday";
-      break;
-    case 5:
-      dayType = "Friday";
-      break;
-    case 6:
-      dayType = "Saturday";
-      break;
-    default:
-      console.log("Error: Your current day is " + currentDay);
-      break;
+const todoItems = [];
+
+app.get("/", (req, res) => {
+  const currentDay = new Date();
+  const options = {
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+    day: "numeric",
   }
 
-  res.render("dayList", { dayType });
+  const dayType = currentDay.toLocaleString("en-US", options);
+
+  res.render("dayList", { dayType, todoItems });
+});
+
+app.post("/", (req, res) => {
+  const { todoTask } = req.body;
+
+  if (todoTask.trim()) todoItems.push(todoTask);
+
+  res.redirect("/")
 })
 
 app.listen(3002, () => console.log("Server running on port 3002"));
