cde155a6a0a8a239a334682e6dcb2288ddc4fdc5
bunnyBites <s.mukeshkr481@gmail.com>
2023-03-13 12:28:16 +0530

    added dayType viewer using ejs

--- app.js
+++ app.js
@@ -5,8 +5,41 @@ const ejs = require("ejs");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// setting current view engine as ejs
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  const currentDay = new Date().getDay();
+  let dayType = "";
+
+  switch (currentDay) {
+    case 0:
+      dayType = "Sunday";
+      break;
+    case 1:
+      dayType = "Monday";
+      break;
+    case 2:
+      dayType = "Tuesday";
+      break;
+    case 3:
+      dayType = "Wednesday";
+      break;
+    case 4:
+      dayType = "Thursday";
+      break;
+    case 5:
+      dayType = "Friday";
+      break;
+    case 6:
+      dayType = "Saturday";
+      break;
+    default:
+      console.log("Error: Your current day is " + currentDay);
+      break;
+  }
+
+  res.render("dayList", { dayType });
 })
 
 app.listen(3002, () => console.log("Server running on port 3002"));
