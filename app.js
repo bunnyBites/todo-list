83434c8989f1b6417943527e7cc1166048da8897
bunnyBites <s.mukeshkr481@gmail.com>
2023-03-18 13:02:11 +0530

    use mongoose to create item and display

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const util = require(__dirname + "/util");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect with mongoose
+mongoose.connect("mongodb://127.0.0.1:27017/todoListDB").then((value) => {
+  if (value) console.log("Connection to Mongo Database is Successful!");
+});
 
 // setting current view engine as ejs
 app.set("view engine", "ejs");
@@ -12,34 +16,43 @@ app.set("view engine", "ejs");
 // set public folder as static file holder
 app.use(express.static("public"));
 
-const todoItems = [];
-const workItems = [];
+const port = process.env.PORT || 3002;
 
-const port = (process.env.PORT || 3002);
+const itemSchema = new mongoose.Schema({ name: String });
+
+// Create a model based on the schema created
+const Item = new mongoose.model("Item", itemSchema);
 
 app.get("/", (req, res) => {
-  const dayType = util.getCurrentDay();
-  res.render("todoList", { todoHeading: dayType, todoItems });
+  Item.find().then((result) => {
+    if (!result.length) {
+      Item.create([{ name: "Eat" }, { name: "Sleep" }, { name: "Code" }])
+        .then(() => {
+          console.log("Inserted Default todo list Successfully!");
+
+          // This will redirect to the onload route and goto else statemement
+          res.redirect("/");
+        });
+    } else {
+      res.render("todoList", { todoHeading: "Yo", todoItems: result });
+    }
+  });
 });
 
 app.post("/", (req, res) => {
   const { todoTask, submitTodoBtn } = req.body;
 
-  if (submitTodoBtn === "Work") {
-    if (todoTask.trim()) workItems.push(todoTask);
-    res.redirect("/work");
-  } else {
-    if (todoTask.trim()) todoItems.push(todoTask);
-    res.redirect("/")
-  }
+  if (todoTask) Item.create({ name: todoTask });
+
+  res.redirect("/");
 });
 
 app.get("/work", (req, res) => {
-  res.render("todoList", { todoHeading: "Work Items", todoItems: workItems })
+  res.render("todoList", { todoHeading: "Work Items", todoItems: workItems });
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
-})
+});
 
 app.listen(port, () => console.log("Server running on port " + port));
